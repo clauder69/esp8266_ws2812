73a63566e156f48d94c59e27441e070c7db2ddab
clauder69 <57950770+clauder69@users.noreply.github.com>
2020-07-29 11:22:52 -0300

    Add files via upload

---
+++ NeoPixel_ESP-01.ino
@@ -0,0 +1,95 @@
+// NeoPixelTest
+// This example will cycle between showing four pixels as Red, Green, Blue, White
+// and then showing those pixels as Black.
+//
+// Included but commented out are examples of configuring a NeoPixelBus for
+// different color order including an extra white channel, different data speeds, and
+// for Esp8266 different methods to send the data.
+// NOTE: You will need to make sure to pick the one for your platform 
+//
+//
+// There is serial output of the current state so you can confirm and follow along
+//
+#ifdef ESP8266
+extern "C" {
+#include "user_interface.h"
+#include "i2s_reg.h"
+#include "slc_register.h"
+#include "esp8266_peri.h"
+void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
+}
+#endif
+#include <NeoPixelBus.h>
+
+const uint16_t LED_NUM = 24; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 2;  // make sure to set this to the correct pin, ignored for Esp8266
+
+#define colorSaturation 128
+
+// three element pixels, in different order and speeds
+//NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+//NeoPixelBus<NeoRgbFeature, Neo400KbpsMethod> strip(PixelCount, PixelPin);
+
+// For Esp8266, the Pin is omitted and it uses GPIO3 due to DMA hardware use.  
+// There are other Esp8266 alternative methods that provide more pin options, but also have
+// other side effects.
+// for details see wiki linked here https://github.com/Makuna/NeoPixelBus/wiki/ESP8266-NeoMethods 
+
+// You can also use one of these for Esp8266, 
+// each having their own restrictions
+//
+// These two are the same as above as the DMA method is the default
+// NOTE: These will ignore the PIN and use GPI03 pin
+//NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(PixelCount, PixelPin);
+//NeoPixelBus<NeoRgbFeature, NeoEsp8266Dma400KbpsMethod> strip(PixelCount, PixelPin);
+
+// Uart method is good for the Esp-01 or other pin restricted modules
+// for details see wiki linked here https://github.com/Makuna/NeoPixelBus/wiki/ESP8266-NeoMethods 
+// NOTE: These will ignore the PIN and use GPI02 pin
+NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart1800KbpsMethod> pixels(LED_NUM, PixelPin);
+//NeoPixelBus<NeoRgbFeature, NeoEsp8266Uart1400KbpsMethod> strip(PixelCount, PixelPin);
+
+// The bitbang method is really only good if you are not using WiFi features of the ESP
+// It works with all but pin 16
+//NeoPixelBus<NeoGrbFeature, NeoEsp8266BitBang800KbpsMethod> strip(PixelCount, PixelPin);
+//NeoPixelBus<NeoRgbFeature, NeoEsp8266BitBang400KbpsMethod> strip(PixelCount, PixelPin);
+
+// four element pixels, RGBW
+//NeoPixelBus<NeoRgbwFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+void setup()
+{
+    pixels.Begin();
+    pixels.Show();
+}
+
+int bar = 1;
+int dir = 1;
+int pos = 1;
+int fim = LED_NUM;
+int intensity = 150;
+int intensityDirection = 1;
+
+void loop()
+{
+   pixels.SetPixelColor(pos, RgbColor(intensity)); // Moderately bright green color.
+   pixels.Show(); // This sends the updated pixel color to the hardware.
+   pixels.SetPixelColor(pos+(dir>0?0:bar), RgbColor(0)); // Moderately bright green color.
+   if (true) {
+     pos += dir;
+     if (pos<=0 || pos>=fim) {
+        if (pos<=0) {
+           dir = 1;
+           pos = 0;
+        } else {
+           dir = -1;
+           pos = fim;
+        }
+     }
+     delay(100);
+   } else {
+     intensity+=intensityDirection;
+     if (intensity <=0 || intensity >= 250)
+        intensityDirection = intensityDirection*-1;
+     delay(5);
+   }
+}
